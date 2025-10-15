94eace08307f443b46ae3cc842547fccedcedda1
Dixit <drdixit6@gmail.com>
2025-10-15 10:08:01 +0530

    init

---
+++ bin/console_project.dart
@@ -0,0 +1,5 @@
+import '../lib/console_project.dart' as console_project;
+
+void main(List<String> arguments) {
+  print('Hello world: ${console_project.calculate()}!');
+}
---
+++ test/console_project_test.dart
@@ -0,0 +1,8 @@
+import '../lib/console_project.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
